418620c5a462c1d78947672684ad77db6741ab2a
StirlitzMuller1489 <114472400+StirlitzMuller1489@users.noreply.github.com>
2023-02-08 15:05:59 +0700

    Update решения.py

--- 2полугодие/решения.py
+++ 2полугодие/решения.py
@@ -1,3 +1,10 @@
+#2
+for x in range(0,2):
+    for y in range(0,2):
+        for z in range(0,2):
+            for w in range(0,2):
+                if (not(y<=x) or (z<=w) or not(z))==0:
+                    print(y,x,z,w)
 
 #6
 s=0
